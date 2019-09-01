8e0af4e8cb59657e620e664ef680571fc7259afc
Dan <dangh32@yahoo.com>
2019-09-01 22:04:22 +0300

    Updates

---
+++ precache-manifest.9f05d90d160c3f2257c7361c532ba605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7ba1860b82428039050ae60f63c242",
+    "url": "/react-tutorial-tania/index.html"
+  },
+  {
+    "revision": "1c2c751db2567c57aabf",
+    "url": "/react-tutorial-tania/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/react-tutorial-tania/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "1c2c751db2567c57aabf",
+    "url": "/react-tutorial-tania/static/js/main.d95b171b.chunk.js"
+  },
+  {
+    "revision": "00ba6a7c9206d6a232d2",
+    "url": "/react-tutorial-tania/static/js/runtime~main.262fd8b0.js"
+  }
+]);
